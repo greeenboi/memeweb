24772b81c5708caa4da2bd5e5f1f7f1b01160ff2
greeenboi <suvan.gowrishanker.204@gmail.com>
2023-08-28 16:11:24 +0530

    final change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className='flex flex-row min-h-12 justify-center items-center mb-2 pb-2 md:gap-12 gap-6'>
      <a href="https://github.com/greeenboi"><AiOutlineGithub/></a>
      <a href="https://www.linkedin.com/in/suvan-gowri-shanker-596943261/"><AiOutlineLinkedin/></a>
-     <a href="https://heylink.me/green47/"><AiOutlineLink/></a>
+     <a href="https://heylink.me/green47/"><AiOutlineLink/></a> 
     </div>
   )
 }
