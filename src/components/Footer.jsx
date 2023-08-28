e1abcdebdecd0110430b865e7004ddc542914685
greeenboi <suvan.gowrishanker.204@gmail.com>
2023-08-28 16:09:50 +0530

    final changes to website

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { AiOutlineGithub, AiOutlineLinkedin, AiOutlineLink } from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='flex flex-row min-h-12 justify-center items-center mb-2 pb-2 md:gap-12 gap-6'>
+     <a href="https://github.com/greeenboi"><AiOutlineGithub/></a>
+     <a href="https://www.linkedin.com/in/suvan-gowri-shanker-596943261/"><AiOutlineLinkedin/></a>
+     <a href="https://heylink.me/green47/"><AiOutlineLink/></a>
+    </div>
   )
 }
 
